548f3408759ae1b9bb33f4fc5526247a4b420f32
Om Rajpurkar <om.sam1999@gmail.com>
2019-12-26 17:44:09 +0530

    Minor Changes

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,6 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-
 STATUS = (
     (0,"Draft"),
     (1,"Publish")
