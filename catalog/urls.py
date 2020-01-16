6494aacd9efb6221bea5765ecc93f30a494bd172
Priteshkamde <priteshkamde24@gmail.com>
2020-01-16 12:09:58 +0530

    ui changes - nav bar added

--- catalog/urls.py
+++ catalog/urls.py
@@ -20,6 +20,7 @@ from catalog import views
 
 urlpatterns = [
     path('', views.IndexPageView.as_view(), name='index'),
+    path('index/', views.IndexPageView.as_view(), name='index'),
     path('aboutus/', views.AboutUsPageView.as_view(), name='aboutus'),
     path('whatwedo/', views.WhatWeDoPageView.as_view(), name='whatwedo'),
     path('products/', views.ProductsPageView.as_view(), name='products'),
