1ac82ec4d90d3ccec2a5c33a351fc2819a1c5954
Rushike Supreme <rushike.ab1@gmail.com>
2020-01-20 12:14:26 +0530

    -postgress database configured

--- helios/settings.py
+++ helios/settings.py
@@ -153,10 +153,19 @@ WSGI_APPLICATION = 'helios.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'algonauts',
+        'USER': 'algonauts',
+        'PASSWORD': 'okokok99',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
 }
 
 # Password validation
