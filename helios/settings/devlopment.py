a2d707df705d88d335d83cd894daef01b06269c4
Rushike Supreme <rushike.ab1@gmail.com>
2020-01-22 16:11:49 +0530

    settings py changes for devlopment and production enviroment

---
+++ helios/settings/devlopment.py
@@ -0,0 +1,10 @@
+from helios.settings.common import *
+
+SECURE_SSL_REDIRECT = False
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'dev.algonauts.in',
+    'algonauts.in',
+]
---
+++ helios/settings/production.py
@@ -0,0 +1,8 @@
+from helios.settings.common import *
+
+SECURE_SSL_REDIRECT = True
+
+ALLOWED_HOSTS = [
+    'dev.algonauts.in',
+    'algonauts.in',
+]
