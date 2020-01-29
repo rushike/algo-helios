85664d0fe2268837db539f4375ac446c072d1115
Rushike Supreme <rushike.ab1@gmail.com>
2020-01-29 15:41:46 +0530

    attribute err, removed unwated database saves

--- users/models.py
+++ users/models.py
@@ -194,13 +194,13 @@ def create_individual_user_group(sender, instance, **kwargs):
 	# group.save()
 	if group is None : return
 	group_map = UserGroupMapping.objects.create_user_group_mapping(user_group_id = group, user_profile_id = instance, delta_period= 4, group_admin = True)
-	group_map.save()
-	return
+	# group_map.save()
+	# return
 
 def create_ug_mapping(sender, instance, **kwargs):
 	group_map = UserGroupMapping.objects.create_user_group_mapping(user_group_id = instance, user_profile_id = instance.admin, delta_period= 4, group_admin = True)
-	group_map.save()
-	return
+	# group_map.save()
+	# return
 
 # DB Signals 
 post_save.connect(create_individual_user_group, sender=AlgonautsUser, dispatch_uid="users.models.AlgonautsUser") # to create users individual group after user creation
