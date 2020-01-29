661ec1366c47d779467c956ff9bf6c47ccd5f380
pritesh kamde <priteshkamde24@gmail.com>
2020-01-29 15:43:23 +0530

    smalll changes

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -13,35 +13,48 @@ def plan_overview(request, slug):
 def plan_subscribe(request):
     subs_attr = dict(request.POST.lists()) 
     subs_attr['email'] = request.user.email #get users email
-
+    ru = request.user
     # user_plan is an array type
     user_plan = Plan.objects.filter(plan_name=subs_attr['plan_name'][0])[0] 
 
     #one user linked with multiple groups
-    user_all_groups = UserGroupMapping.objects.filter(user_profile_id=request.user)
+    # user_all_groups = UserGroupMapping.objects.filter(user_profile_id=request.user)
     
 
-    u_gid = None
-    for i in range(user_all_groups.count()):
+    ne = UserGroup.objects.create_user_group(user_plan.user_group_type_id, datetime.datetime.now(), admin=request.user)
+    ugti = user_plan.user_group_type_id.id
+    usermpa = UserGroupMapping.objects.all()
+    u_gid_____ = UserGroupMapping.objects.all().values('user_group_id','user_profile_id','user_group_id__user_group_type_id') 
+    u_gid = u_gid_____.filter(user_profile_id=request.user, user_group_id__user_group_type_id=user_plan.user_group_type_id)
+
+    u_g = UserGroup.objects.get(id=u_gid[0])
+    # import logging
+    # import logging.config
+    
+    # logging.config.fileConfig('logging.conf')
+    # logger = logging.getLogger('applog')
+    # logger.debug(str(u_gid)+"prit")
+
+    # print(u_gid)
+
+    # for i in range(user_all_groups.count()):
         
-        if user_all_groups[i].user_group_id.user_group_type_id == user_plan.user_group_type_id:
-            u_gid = user_all_groups[i].user_group_id
-            break
+    #     if user_all_groups[i].user_group_id.user_group_type_id == user_plan.user_group_type_id:
+    #         u_gid = user_all_groups[i].user_group_id
+    #         break
     
     
     # saving all the stuff 
 
     Subscription.objects.create(
-        user_group_id = u_gid,
+        user_group_id = u_g,
         plan_id = user_plan,
         subscription_start = datetime.datetime.now(),
         subscription_end = datetime.datetime.now() + datetime.timedelta(days=1),
         subscription_active = True,
         payment_id = 0, 
-        trial_subscription_start = datetime.datetime.now(),
-        trial_subscription_end = datetime.datetime.now() + datetime.timedelta(days=1)
     ).save()
-
+    
     return HttpResponse(u_gid)
 
 
--- users/models.py
+++ users/models.py
@@ -195,12 +195,12 @@ def create_individual_user_group(sender, instance, **kwargs):
 	if group is None : return
 	group_map = UserGroupMapping.objects.create_user_group_mapping(user_group_id = group, user_profile_id = instance, delta_period= 4, group_admin = True)
 	# group_map.save()
-	# return
+	return
 
 def create_ug_mapping(sender, instance, **kwargs):
 	group_map = UserGroupMapping.objects.create_user_group_mapping(user_group_id = instance, user_profile_id = instance.admin, delta_period= 4, group_admin = True)
 	# group_map.save()
-	# return
+	return
 
 # DB Signals 
 post_save.connect(create_individual_user_group, sender=AlgonautsUser, dispatch_uid="users.models.AlgonautsUser") # to create users individual group after user creation
