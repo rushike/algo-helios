9177bb5cce4f7c541e9737df453b2216f0a9df19
pritesh kamde <priteshkamde24@gmail.com>
2020-02-11 11:24:11 +0530

    Again Spacing & Tabs

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from catalog import views
 
 urlpatterns = [
-    # path('', views.HomeRedirect, name='home_redirect'),
     path('', views.IndexPageView.as_view(), name='index'),
     path('aboutus/', views.AboutUsPageView.as_view(), name='aboutus'),
     path('whatwedo/', views.WhatWeDoPageView.as_view(), name='whatwedo'),
