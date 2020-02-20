f90e006d52d1dd7b11ef02594eaacb03cfa4451c
Gaurav Mane <admin@algonauts.onmicrosoft.com>
2020-02-20 20:57:24 +0530

    Change in Django Settings

--- helios/settings/production.py
+++ helios/settings/production.py
@@ -8,6 +8,55 @@ ALLOWED_HOSTS = [
 ]
 
 SECURE_SSL_REDIRECT = True
+
+ADMINS = [('Gaurav', 'gaurav.mane@algonauts.in')]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': "[{asctime}] {levelname} {module} [{name}:{lineno}] {process:d} {thread:d} {message}",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue'
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file', 'mail_admins'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'worker': {
+            'handlers': ['file', 'mail_admins'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
 CRONJOBS = [
     ('0 0 * * *', 'subscriptions.cron.check_data_consistency')
 ]
